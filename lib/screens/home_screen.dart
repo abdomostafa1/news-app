a796f9e254ccc9eedae41d7068d9b51ca39d8fd7
abdo mostafa <abdomostafa19999@gmail.com>
2024-04-04 09:53:14 +0200

    using custom scrollview and slivers

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,40 +10,39 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'News',
-              style:
-                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Cloud',
-              style:
-                  TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold),
-            )
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          const CategoryListView(),
-          const SizedBox(
-            height: 16,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'News',
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Cloud',
+                style: TextStyle(
+                    color: Colors.yellow, fontWeight: FontWeight.bold),
+              )
+            ],
           ),
-          Expanded(
-            child: ListView.builder(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return const NewsItem();
-                }),
-          )
-        ],
-      ),
-    );
+        ),
+        body:  CustomScrollView(
+          physics: RangeMaintainingScrollPhysics(),
+          slivers: [
+            const SliverToBoxAdapter(child: CategoryListView()),
+            const SliverToBoxAdapter(
+              child: SizedBox(
+                height: 16,
+              ),
+            ),
+            SliverList.builder(itemCount: 10, itemBuilder: (context, index) {
+              return const NewsItem();
+            })
+          ],
+        )
+
+        );
   }
 }
